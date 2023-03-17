ff0ae0dd79f6c61510d5dfc970c80a3872fdc0bc
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2023-03-17 16:51:58 +0100

    update mongodb connection file

--- back_end/db.js
+++ back_end/db.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const MONGO_URL= process.env.MONGO_URL
 
 function connectToMongoDB() {
-	// mongoose.set("strictQuery", false);
+
 	mongoose.connect(MONGO_URL);
 
     mongoose.connection.on('connected', () => {
